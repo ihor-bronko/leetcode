8a7edd0b7664a24db9ab0a2a5884abf560e6a90b
Suric1 <igorbronko@gmail.com>
2022-10-19 00:22:57 +0300

    Create 27. Remove Element

---
+++ Difficulty - Easy/Remove Element.js
@@ -0,0 +1,12 @@
+// 27. Remove Element
+
+const removeElement = (nums, val) => {
+	let index = 0
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] !== val) {
+			nums[index] = nums[i]
+			index++
+		}
+	}
+	return index
+}
