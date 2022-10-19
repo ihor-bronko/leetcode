62d401601d8d545fddb9db453fd4862b96581b07
Suric1 <igorbronko@gmail.com>
2022-10-19 12:40:30 +0300

    Create 66. Plus One

---
+++ Difficulty - Easy/Plus One.js
@@ -0,0 +1,17 @@
+// 66. Plus One
+
+const plusOne = digits => {
+	for (let i = digits.length - 1; i >= 0; i--) {
+		if (digits[i] < 9) {
+			digits[i] = digits[i] + 1
+			return digits
+		} else {
+			digits[i] = 0
+		}
+	}
+	digits.unshift(1)
+	return digits
+}
+
+console.log(plusOne([9]))
+console.log(plusOne([1, 2, 3]))
